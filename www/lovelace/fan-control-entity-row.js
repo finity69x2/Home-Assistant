e09d7d2f0ffe383aae250db3f8b8c471fc4af5d3
finity69x2 <32221243+finity69x2@users.noreply.github.com>
2019-03-02 15:40:34 -0500

    Create fan-control-entity-row.js

---
+++ www/lovelace/fan-control-entity-row.js
@@ -0,0 +1,168 @@
+class CustomFanRow extends Polymer.Element {
+
+    static get template() {
+        return Polymer.html`
+            <style is="custom-style" include="iron-flex iron-flex-alignment"></style>
+            <style>
+                :host {
+                    line-height: inherit;
+                }
+                .speed {
+                    min-width: 30px;
+					max-width: 30px;
+					height: 30px;
+					margin-left: 2px;
+                    margin-right: 2px;
+    	            background-color:#759aaa;
+	                border: 1px solid lightgrey; 
+					border-radius: 4px;
+	                font-size: 10px !important;
+					color: inherit;
+					text-align: center;
+	                float: right !important;
+					padding: 1px;
+				}
+				
+               </style>
+            <hui-generic-entity-row hass="[[hass]]" config="[[_config]]">
+                <div class='horizontal justified layout' on-click="stopPropagation">
+                    <button
+						class='speed'
+						style='[[_lowOnColor]]'
+						toggles name="low"
+						on-click='setSpeed'
+                        disabled='[[_isOnLow]]'>LOW</button>
+                    <button
+						class='speed'
+                        style='[[_medOnColor]]'
+                        toggles name="medium"
+                        on-click='setSpeed'
+                        disabled='[[_isOnMed]]'>MED</button>
+                    <button
+						class='speed'
+                        style='[[_highOnColor]]'
+                        toggles name="high"
+                        on-click='setSpeed'
+                        disabled='[[_isOnHigh]]'>HIGH</button>
+				    <button
+						class='speed'
+                        style='[[_offColor]]'
+                        toggles name="off"
+                        on-click='setSpeed'
+                        disabled='[[_isOffState]]'>OFF</button>
+                </div>
+            </hui-generic-entity-row>
+        `;
+    }
+
+    static get properties() {
+        return {
+            hass: {
+                type: Object,
+                observer: 'hassChanged'
+            },
+            _config: Object,
+            _stateObj: Object,
+			_lowOnColor: String,
+			_medOnColor: String,
+			_highOnColor: String,
+			_offColor: String,
+            _isOffState: Boolean,
+            _isOnState: Boolean,
+            _isOnLow: Boolean,
+            _isOnMed: Boolean,
+            _isOnHigh: Boolean
+        }
+    }
+
+    setConfig(config) {
+        this._config = config;
+    }
+
+    hassChanged(hass) {
+
+        const config = this._config;
+        const stateObj = hass.states[config.entity];
+
+        let speed;
+        if (stateObj && stateObj.attributes) {
+            speed = stateObj.attributes.speed || 'off';
+        }
+		
+		let low;
+		let med;
+		let high;
+		let offstate;
+		
+		if (stateObj && stateObj.attributes) {
+		    if (stateObj.state == 'on' && stateObj.attributes.speed == 'low') {
+			    low = 'on';
+		    } else if (stateObj.state == 'on' && stateObj.attributes.speed == 'medium') {
+			    med = 'on';
+		    } else if (stateObj.state == 'on' && stateObj.attributes.speed == 'high') {
+			    high = 'on';
+		    } else {
+				offstate = 'on';
+			}
+		}
+		
+        let lowcolor;
+		let medcolor;
+		let hicolor;
+		let offcolor;
+		
+		if (low == 'on') {
+			lowcolor = 'background-color: #43A047';
+		} else {
+			lowcolor = '';
+		}
+		
+		if (med == 'on') {
+			medcolor = 'background-color: #43A047';
+		} else {
+			medcolor = '';
+		}
+		
+		if (high == 'on') {
+			hicolor = 'background-color: #43A047';
+		} else {
+			hicolor = '';
+		}
+		
+		if (offstate == 'on') {
+			//offcolor = 'background-color: #43A047';
+			offcolor = 'background-color: #f44c09';
+		} else {
+			offcolor = '';
+		}
+		
+		this.setProperties({
+            _stateObj: stateObj,
+			_isOffState: stateObj.state == 'off',
+            _isOnLow: low === 'on',
+			_isOnMed: med === 'on',
+			_isOnHigh: high === 'on',
+			_lowOnColor: lowcolor,
+			_medOnColor: medcolor,
+			_highOnColor: hicolor,
+			_offColor: offcolor
+        });
+    }
+
+    stopPropagation(e) {
+        e.stopPropagation();
+    }
+
+    setSpeed(e) {
+        const speed = e.currentTarget.getAttribute('name');
+        if( speed == 'off' ){
+			this.hass.callService('fan', 'turn_off', {entity_id: this._config.entity});
+			this.hass.callService('fan', 'set_speed', {entity_id: this._config.entity, speed: speed});
+       	} else {
+			this.hass.callService('fan', 'set_speed', {entity_id: this._config.entity, speed: speed});
+		}
+    }
+		
+}
+
+customElements.define('fan-control-entity-row', CustomFanRow);
