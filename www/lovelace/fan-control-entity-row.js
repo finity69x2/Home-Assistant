92e6912817b7de72090ede96a3b35a05038c8342
finity69x2 <32221243+finity69x2@users.noreply.github.com>
2019-03-02 16:42:43 -0500

    Update fan-control-entity-row.js

--- www/lovelace/fan-control-entity-row.js
+++ www/lovelace/fan-control-entity-row.js
@@ -156,7 +156,7 @@ class CustomFanRow extends Polymer.Element {
     setSpeed(e) {
         const speed = e.currentTarget.getAttribute('name');
         if( speed == 'off' ){
-			this.hass.callService('fan', 'turn_off', {entity_id: this._config.entity});
+			//this.hass.callService('fan', 'turn_off', {entity_id: this._config.entity});
 			this.hass.callService('fan', 'set_speed', {entity_id: this._config.entity, speed: speed});
        	} else {
 			this.hass.callService('fan', 'set_speed', {entity_id: this._config.entity, speed: speed});
